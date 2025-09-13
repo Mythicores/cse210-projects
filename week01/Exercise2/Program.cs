cf7df6d494dce54eca4b616d6db4d285d10b7ebd
Mythicores <nathjone9675@gmail.com>
2025-09-13 17:42:22 -0600

    Week 1: Exercise 2 Added grading system
    Added a grade calculator based on your grade percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please write your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got {letter}!");
+
+        if (grade > 70)
+        {
+            Console.Write("Congratulations! You passed!");
+        }
+
+
+        if (grade < 70)
+            {
+                Console.Write("Better luck next time!");
+            }
+
     }
 }
