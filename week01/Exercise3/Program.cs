fb48a1fe2e2f1b800dd1e01bb03019b227417601
Mythicores <nathjone9675@gmail.com>
2025-09-13 21:12:33 -0600

    Week 1: Exercise 3 - Added number guessing game
    I added a number guessing game that generates a random number from 1 to 100. It lets you know whether the number should be higher or lower. Unlimited guess.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //string input = Console.ReadLine();
+        //int number = int.Parse(input);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        string GuessInput = Console.ReadLine();
+        int guess = int.Parse(GuessInput);
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            GuessInput = Console.ReadLine();
+            guess = int.Parse(GuessInput);
+
+        }
+        if (guess == number)
+        {
+           Console.WriteLine("Congrats! You guessed right!");
+        }
+        
     }
+    
 }
