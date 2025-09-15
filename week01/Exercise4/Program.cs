ac57672a38a50ad41d101e008ae0af05be866759
Mythicores <nathjone9675@gmail.com>
2025-09-15 16:41:00 -0600

    Week 1: Exercise 4 - Added number adding calculator that computs the total sum of number entered, the average, and the largest number of them.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers. When you're done, type 0");
+        List<int> TotalNumbers = new List<int>();
+        int sum = 0;
+        int number = 0;
+        int loops = 0;
+        float Average = 0.0F;
+        int Largest = 0;
+        do
+        {
+
+            Console.Write("Enter a number: ");
+            string UserInput = Console.ReadLine();
+            number = int.Parse(UserInput);
+            TotalNumbers.Add(number);
+            sum = sum + number;
+            loops = loops + 1;
+            if (Largest < number)
+            {
+                Largest = number;
+            }
+
+        } while (number != 0);
+        loops = loops - 1;
+        Average = sum / loops;
+        Console.WriteLine($"Total sum: {sum}");
+        Console.WriteLine($"Average: {Average}");
+        Console.WriteLine($"Largest number: {Largest}");
+
     }
 }
