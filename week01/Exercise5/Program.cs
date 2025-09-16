777fcc204e8eac7586d19e25c2d2007dc19618c3
Mythicores <nathjone9675@gmail.com>
2025-09-15 19:49:57 -0600

    Week 1: Exercise 5 - Added prompt and display for username and user's favorite number.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string UserName = PromptUserName("What's your name? ");
+        int UserNumber = PromptUserNumber("What's your favorite number? ");
+        SquareNumber(UserNumber);
+        DisplayResult(UserName, UserNumber);
     }
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName(string UserName)
+        {
+            Console.Write($"{UserName}");
+            UserName = Console.ReadLine();
+            return UserName;
+        }
+        static int PromptUserNumber(string UserNumber)
+        {
+            Console.Write($"{UserNumber}");
+            string StringNumber = Console.ReadLine();
+            int number = int.Parse(StringNumber);
+            return number;
+        }
+        static int SquareNumber(int SquaredNumber)
+        {
+            SquaredNumber ^= 2;
+            return SquaredNumber;
+        }
+        static void DisplayResult(string UserName, int SquaredNumber)
+        {
+            Console.WriteLine($"User: {UserName}");
+            Console.WriteLine($"User's number: {SquaredNumber}");
+        }
 }
